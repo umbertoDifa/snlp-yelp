940149af46d3435fddd0673f66de1f66f96bada4
Vikisix <vittorioselo@me.com>
2015-11-15 16:37:53 -0600

    Changes

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,13 +1,11 @@
 __author__ = 'vittorioselo'
-
 import pandas
 import numpy
 from sklearn import metrics
 import os
 from os.path import join, isfile
 from collections import defaultdict
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.naive_bayes import GaussianNB
 
 
 
@@ -19,11 +17,8 @@ listUsers.remove('.DS_Store')
 
 dictResults = defaultdict(float)
 
-i = 0
 for user in listUsers:
-    i += 1
-    print(i)
-    #======READING TRAIN SET========
+     #======READING TRAIN SET========
     dataTrain = numpy.array(pandas.read_csv('train/'+user, header=None))
     trainRank = numpy.array(pandas.read_csv('train/stars/'+user, header=None))
 
@@ -38,10 +33,9 @@ for user in listUsers:
     testRank = [val for sublist in testRank for val in sublist]
     testRank = list(map(lambda x: int(x*5), testRank))
 
-    forest = RandomForestClassifier(n_estimators=400)
-    forest.fit(dataTrain, trainRank)
-
-    prediction = forest.predict(dataTest)
+    gnb = GaussianNB()
+    gnb.fit(dataTrain, trainRank)
+    prediction = gnb.predict(dataTest)
 
     dictResults[user] = metrics.accuracy_score(testRank, prediction)
 
@@ -53,12 +47,11 @@ accuracy /= len(listUsers)
 
 print(accuracy)
 #MIN REVIEWS 20
-#ACC .416982853394 -> NO TAGS 400 tree
-#ACC .415015311991 -> noun and 100 200 tree
-#ACC .415776112382 -> noun + adj and 200 trees
-#ACC .445425589304 -> noun + adj and 400 trees
-#ACC .44691447588 -> noun + adj and 1000 trees
+# ACC .389963452794 -> NO TAG
+# ACC .332423606817 -> noun
+# ACC .369191294046 -> noun + adjective
+
+#MIN REVIEWS 25
+#ACC .339427339762 -> noun
 
 
-#MIN REVIEWS 25 ONLY NOUND
-#ACC .425177274014
